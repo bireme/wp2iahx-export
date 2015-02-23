119f2eb8aa8ec9122ffe52aa6506ad52427fde78
Wilson Moura <wilsonsmoura@gmail.com>
2015-02-23 13:50:11 -0300

    Remove 'more' variable.

--- feed-iahx.php
+++ feed-iahx.php
@@ -23,7 +23,6 @@ $args = array(
 $posts = get_posts($args);
 
 header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
-$more = 1;
 
 echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
 
