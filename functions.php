652b2273d472e0beddc12a848b2617561fed3bc6
Wilson Moura <wilsonsmoura@gmail.com>
2015-03-11 10:17:19 -0300

    Fix author name display.

--- functions.php
+++ functions.php
@@ -3,9 +3,12 @@
 if ( !function_exists( 'author_format' ) ) {
     function author_format($author) {
         $author = explode(' ', $author);
-        $last_name = array_pop($author);
-        $name = implode(' ', $author);
-        echo ucfirst($last_name) . ', ' . ucfirst($name);
+        if ( count($author) > 1 ) {
+            $last_name = array_pop($author);
+            $name = implode(' ', $author);
+            $au = ucfirst($last_name) . ', ' . ucfirst($name);
+        }
+        echo $au ? $au : ucfirst($author[0]);
     }
 }
 
